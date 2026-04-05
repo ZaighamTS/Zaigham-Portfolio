58154c59797f45ee0785ba34ae27befc811f4ef6
ZaighamRaza <zaigham@tecshield.io>
2026-04-05 15:39:16 +0500

    push again

--- js/games/mystic-gem-quest.js
+++ js/games/mystic-gem-quest.js
@@ -4,7 +4,7 @@
     title: "Mystic Gem Quest",
     description:
       "Gem-matching puzzle adventure framing with clear goals and mobile-friendly sessions.",
-    image: "images/games/mystic-gem.png",
+    image: "images/games/mystic-gems.png",
     published: true,
     playUrl: "https://play.google.com/store/apps/details?id=com.TS.MysticGem",
     linkLabel: "Google Play",
